aa684443b898c404b84121c805d7da3bc494fb9b
Jef Braem <jef.braem@codifly.be>
2021-10-19 19:16:55 +0200

    exercise-6

--- views/Leaderboard.js
+++ views/Leaderboard.js
@@ -6,39 +6,11 @@ import LoadingView from '../components/LoadingView';
 import API from '../global/API';
 import useApiRequest from '../utils/useApiRequest';
 
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-  },
-  scrollView: {
-    paddingHorizontal: 16,
-    height: '100%',
-  }
-});
-
 const Leaderboard = () => {
   const [fetchLeaderboard, { loading, data: results = [] }] = useApiRequest(API.getLeaderboard);
 
-  useEffect(() => {
-    fetchLeaderboard();
-  }, []);
-
-  if (loading) {
-    return <LoadingView />;
-  }
-
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-        {results.map(({ name, position, score, time }) => (
-          <List.Item
-            key={name}
-            title={name}
-            description={`${score || 0}/10 ${(time || 0) / 1000}s`}
-            left={() => <Avatar.Text size={48} label={position} />}
-          />
-        ))}
-      </ScrollView>
+    <View>
     </View>
   );
 };
