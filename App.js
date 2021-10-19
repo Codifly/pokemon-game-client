0063fffd1c3a5501b76252e1b4cb17b89b8212d3
Jef Braem <jef.braem@codifly.be>
2021-10-19 19:39:27 +0200

    exercise-1

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import { KeyboardAvoidingView, ScrollView, StyleSheet } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 import AuthProvider from './global/AuthProvider';
-import Login from './views/Login';
-import Home from './views/Home';
-import Game from './views/Game';
-import Result from './views/Result';
-import Leaderboard from './views/Leaderboard';
-
-console.disableYellowBox = true;
 
 const styles = StyleSheet.create({
   container: {
@@ -29,7 +22,6 @@ export default function App() {
         style={styles.container}
       >
         <StatusBar style="auto" />
-        <Login />
       </KeyboardAvoidingView>
     </AuthProvider>
   );
